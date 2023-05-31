86933e8fe04401e082695ab4b0c18ea4203df0d3
Qiu Daomao <qiudaomao@gmail.com>
2023-05-31 08:02:27 +0800

    Fix svg height
    use 626 fixed height is incorrect

--- Sources/FlameGraphCore/Renderer/SVGRender.swift
+++ Sources/FlameGraphCore/Renderer/SVGRender.swift
@@ -205,7 +205,7 @@ let depthmax = 0;
 let header = """
 <?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg version="1.1" width="1200" height="626" onload="init(evt)" viewBox="0 0 1200 626" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<svg version="1.1" width="\(imagewidth)" height="\(imageheight)" onload="init(evt)" viewBox="0 0 \(imagewidth) \(imageheight)" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
 """
 
 let script = """
